4959558429436d3c15ecb662a8ef4950e14ccc6b
ahart8 <121808111+ahart8@users.noreply.github.com>
2023-04-01 15:51:57 -0400

    Checking in assignment to Git

--- Basic_Git_and_VisualStudio_Application/Basic_Git_and_VisualStudio_Application/wwwroot/js/site.js
+++ Basic_Git_and_VisualStudio_Application/Basic_Git_and_VisualStudio_Application/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+//FUNCTIONS FOR ANIMATION
+var id = null;
+function myMove() {
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
